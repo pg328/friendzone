3e9014c20eb46ce85ba580f0e90489da6af0d057
Philip George <23001002+pg328@users.noreply.github.com>
2023-03-01 19:18:55 +0000

    tailwind preact and sanity integrated

--- projects/astro/astro.config.mjs
+++ projects/astro/astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
+import sanity from 'astro-sanity'
+import sanityConfig from 'api/src/sanity/sanity.config'
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [mdx(), tailwind()],
+	integrations: [
+    mdx(), 
+    tailwind(), 
+    sanity(sanityConfig)],
 });
