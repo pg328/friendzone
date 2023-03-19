1636f001834bbccf5dde8ee4b998e53aa61be816
Philip George <23001002+pg328@users.noreply.github.com>
2023-03-19 18:00:49 +0000

    GPT4

--- packages/solito-common/features/home/screen.tsx
+++ packages/solito-common/features/home/screen.tsx
@@ -1,39 +1,48 @@
-import { A, H1, P, Text, TextLink } from 'solito-common/design/typography'
-import { Row } from 'solito-common/design/layout'
-import { View } from 'solito-common/design/view'
-import React from 'react'
-import { MotiLink } from 'solito/moti'
+import { A, H1, P, Text, TextLink } from 'solito-common/design/typography';
+import { Row } from 'solito-common/design/layout';
+import { View } from 'solito-common/design/view';
+import React from 'react';
+import { MotiLink } from 'solito/moti';
 
 export function HomeScreen() {
   return (
-    <View className="flex-1 items-center justify-center bg-white dark:bg-black p-3">
-      <H1 className="text-black dark:text-white">Welcome to Friendzone.</H1>
-      <View className="max-w-xl ">
+    <View className="flex-1 items-center justify-center bg-gradient-to-br from-green-400 via-pink-400 to-green-400 dark:from-green-800 dark:via-pink-800 dark:to-green-800 p-3">
+      <H1 className="text-white font-bold mb-4">Welcome to Friendzone.</H1>
+      <View className="max-w-xl bg-white dark:bg-gray-800 p-4 rounded-lg">
         <P className="text-center text-black dark:text-white">
-          By clicking "Log In", you agree with our <P className="text-slate-800 font-bold dark:text-slate-200">Terms</P>.
+          By clicking "Log In", you agree with our{' '}
+          <TextLink className="text-green-500 dark:text-green-300 font-bold" href="/terms">
+            Terms
+          </TextLink>
+          . Learn how we process your data in our{' '}
+          <TextLink className="text-green-500 dark:text-green-300 font-bold" href="/privacy">
+            Privacy Policy
+          </TextLink>
+          .
         </P>
-        {/* <P className="text-center text-black dark:text-white">Learn how we process your data</P>*/}
       </View>
       <View className="h-[32px]" />
       <Row className="space-x-8">
         <MotiLink
           href="/login"
           animate={({ hovered, pressed }) => {
-            'worklet'
+            'worklet';
 
             return {
               scale: pressed ? 0.95 : hovered ? 1.1 : 1,
               rotateZ: pressed ? '0deg' : hovered ? '-3deg' : '0deg',
-            }
+            };
           }}
           transition={{
             type: 'timing',
             duration: 150,
           }}
         >
-          <Text selectable={false} className="text-white dark:text-black bg-black dark:bg-orange-300 text-3xl p-20 rounded-3xl font-bold">Log In</Text>
+          <Text selectable={false} className="text-white dark:text-black bg-black dark:bg-orange-300 text-3xl p-20 rounded-3xl font-bold">
+            Log In
+          </Text>
         </MotiLink>
       </Row>
     </View>
-  )
+  );
 }
--- packages/solito-common/features/login/screen.tsx
+++ packages/solito-common/features/login/screen.tsx
@@ -1,20 +1,47 @@
 
 import { A, H1, P, Text, TextLink } from 'solito-common/design/typography'
 import { Row } from 'solito-common/design/layout'
+import { TextInput, TouchableOpacity } from 'react-native';
 import { View } from 'solito-common/design/view'
-import PhoneInput from 'react-native-phone-number-input'
 import React, { useRef, useState } from 'react'
 import { MotiLink } from 'solito/moti'
 
-export function LoginScreen() {
-  const phoneInput = useRef<PhoneInput>(null)
-  const [value, setValue] = useState("");
-  const [formattedValue, setFormattedValue] = useState("");
-  const [valid, setValid] = useState(false);
-  const [showMessage, setShowMessage] = useState(false);
+export const LoginScreen = () => {
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [verificationCode, setVerificationCode] = useState('');
+
+  const handleLogin = () => {
+    // Implement your phone number authentication and 2FA here
+  };
+
+  const handleVerificationCode = () => {
+    // Implement your 2FA code verification here
+  };
+
   return (
-    <View className="flex-1 items-center justify-center bg-orange-300 dark:bg-rose-900 p-3">
-      <H1 className="text-rose-900 dark:text-orange-300">Log In</H1>
+    <View className={'flex-1 justify-center items-center bg-white'}>
+      <Text className={'text-2xl font-bold text-blue-600'}>Welcome to the App!</Text>
+
+      <TextInput
+        className={'w-full p-2 my-3 border-b border-blue-400'}
+        placeholder="Phone Number"
+        keyboardType="phone-pad"
+        onChangeText={text => setPhoneNumber(text)}
+        value={phoneNumber}
+      />
+
+      <TextInput
+        className={'w-full p-2 my-3 border-b border-blue-400'}
+        placeholder="Verification Code"
+        keyboardType="number-pad"
+        onChangeText={text => setVerificationCode(text)}
+        value={verificationCode}
+      />
+
+      <TouchableOpacity onPress={handleLogin} className={'py-2 px-4 my-3 bg-blue-600 rounded'}>
+        <Text className={'text-white'}>Login</Text>
+      </TouchableOpacity>
     </View>
-  )
-}
+  );
+};
+
